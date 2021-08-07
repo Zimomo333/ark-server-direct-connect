7ff742d97c87f11166b75e0ee7d06cf715dce02d
Zimomo333 <lzh120908717@gmail.com>
2021-08-07 21:47:53 +0800

    update: 按需加载antdv

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,20 +1,51 @@
 <template>
   <div id="app">
-    <landing-page></landing-page>
+    <a-button type="primary" size="large">test</a-button>
+    <h1>PVE</h1>
+      <a href="steam://connect/zimomo.hopto.org:27015">仙境</a>
+      <a href="steam://connect/zimomo.hopto.org:27016">孤岛</a>
+      <a href="steam://connect/zimomo.hopto.org:27017">中心岛</a>
+      <a href="steam://connect/zimomo.hopto.org:27018">瓦尔盖罗</a>
+      <a href="steam://connect/zimomo.hopto.org:27019">灭绝</a>
+      <a href="steam://connect/zimomo.hopto.org:37015">畸变</a>
+      <a href="steam://connect/zimomo.hopto.org:37016">焦土</a>
+      <a href="steam://connect/zimomo.hopto.org:37017">创世纪</a>
+      <a href="steam://connect/zimomo.hopto.org:37021">创世纪2</a>
+      <a href="steam://connect/zimomo.hopto.org:37018">水晶岛</a>
+    <h1>PVP角斗场</h1>
+      <a href="steam://connect/zimomo.hopto.org:37019">中心岛</a>
+      <a href="steam://connect/zimomo.hopto.org:37020">孤岛</a>
   </div>
 </template>
 
 <script>
-  import LandingPage from '@/components/LandingPage'
-
-  export default {
-    name: 'ark-server-direct-connect',
-    components: {
-      LandingPage
-    }
-  }
+export default {
+  name: 'ark-server-direct-connect',
+  components: {}
+}
 </script>
 
 <style>
-  /* CSS */
+html, body{
+  margin: 0;
+}
+#app {
+  margin: 0;
+}
+h1 {
+  font-size: 60px;
+  text-align: center;
+  color: red;
+}
+a {
+  font-size: 60px;
+  width: 400px;
+  text-align: center;
+}
+div {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin: 30px;
+}
 </style>
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
-
+import { Button } from 'ant-design-vue'
 import App from './App'
+
+Vue.component(Button.name, Button)
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
